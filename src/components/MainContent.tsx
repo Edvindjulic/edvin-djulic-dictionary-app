df9da2bf06d2b38a098a405a4f29304111339697
edvindjulic <djulicedvin@gmail.com>
2023-12-03 22:34:16 +0100

    Commented code

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,8 +9,12 @@ interface MainContentProps {
   onClose: () => void;
 }
 
+// This component contains the FavoriteDrawer component and the RenderSearchResult component.
+// isOpen and onClose are props passed from the App component (the parent).
 export default function MainContent({ isOpen, onClose }: MainContentProps) {
+  // Using the useContext hook to access the savedWords and removeWord from the SearchContext.
   const { savedWords, removeWord } = useContext(SearchContext);
+  // FavoriteDrawer's isOpen, onClose, savedWords and removeWord are passed as props.
   return (
     <VStack>
       <FavoriteDrawer
