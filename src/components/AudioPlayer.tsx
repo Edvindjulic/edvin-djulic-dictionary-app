3ccbd39191265a8cc7d441c2fe22e623ebca6ce0
edvindjulic <djulicedvin@gmail.com>
2023-12-03 22:26:19 +0100

    Commented code

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -7,10 +7,13 @@ interface AudioPlayerProps {
   src: string;
 }
 
+// This is a custom component that uses the HTML5 audio element to play the audio of the word.
 export default function AudioPlayer({ src }: AudioPlayerProps) {
+  // Using the useState hook to keep track of whether the audio is playing or not.
   const [isPlaying, setIsPlaying] = useState(false);
+  // Using the useRef hook to access the audio element.
   const audioRef = useRef<HTMLAudioElement>(null);
-
+  // This function toggles between playing and pausing the audio.
   const togglePlayPause = () => {
     if (audioRef.current) {
       if (isPlaying) {
@@ -21,7 +24,7 @@ export default function AudioPlayer({ src }: AudioPlayerProps) {
       setIsPlaying(!isPlaying);
     }
   };
-
+  // This function is called when the audio ends. It sets the isPlaying state to false.
   const handleAudioEnd = () => {
     setIsPlaying(false);
   };
