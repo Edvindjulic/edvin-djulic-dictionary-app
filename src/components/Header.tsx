d6ef056519cce68e7624c1136f2959a280d65bce
edvindjulic <djulicedvin@gmail.com>
2023-12-03 22:24:51 +0100

    Commented code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,12 @@ interface HeaderProps {
 }
 
 export default function Header({ onOpen, btnRef }: HeaderProps) {
+  // Using the useColorMode from Chakra UI to toggle between light and dark mode.
   const { colorMode, toggleColorMode } = useColorMode();
+  // Depending on the color mode, the button text will change.
   const buttonText = colorMode === "light" ? "Dark" : "Light";
+  // The Header component contains the SubmitSearch component, the button to open the drawer with favorite words
+  // and the button to toggle between light and dark mode.
   return (
     <HStack p={5} justify={"center"} alignItems={"center"}>
       <SubmitSearch />
